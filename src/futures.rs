e0c4ec829bad75d8f5f4e6b3fa70038ce6296c8a
Sean <sa.roberts.b@gmail.com>
2024-03-14 19:05:26 +0000

    wake

--- src/futures.rs
+++ src/futures.rs
@@ -16,11 +16,14 @@ where
 
     fn poll(
         mut self: pin::Pin<&mut Self>,
-        _cx: &mut core::task::Context<'_>,
+        cx: &mut core::task::Context<'_>,
     ) -> core::task::Poll<Self::Output> {
         match (self.function)() {
             Ok(value) => core::task::Poll::Ready(Ok(value)),
-            Err(nb::Error::WouldBlock) => core::task::Poll::Pending,
+            Err(nb::Error::WouldBlock) => {
+                cx.waker().wake_by_ref();
+                core::task::Poll::Pending
+            },
             Err(nb::Error::Other(e)) => core::task::Poll::Ready(Err(e)),
         }
     }
