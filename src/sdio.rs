493189de4fe4a73780a8c8d84a89c7a3d5706528
Kyle Pereira <kyle@kylepereira.me>
2025-04-10 14:11:06 +0100

    Log: Remove potential panic! when multiple log()s are performed concurrently

--- src/sdio.rs
+++ src/sdio.rs
@@ -11,15 +11,20 @@ use sequential_storage::{map, queue};
 use stm32f4xx_hal as hal;
 use storage_types::ConfigKey;
 
+use crate::futures::YieldFuture;
 use crate::{CAPACITY, CONFIG_FLASH_RANGE, LOGS_FLASH_RANGE, RTC};
 
-static mut LOGGER: Option<Logger> = Some(Logger { flash: Mutex::new(RefCell::new(None)) });
+static mut LOGGER: Option<Logger> = Some(Logger {
+    flash: Mutex::new(RefCell::new(None)),
+});
 
 pub fn setup_logger<'a>(
     flash: W25QSequentialStorage<Bank1, CAPACITY>,
 ) -> Result<(), embedded_sdmmc::Error<hal::sdio::Error>> {
     unsafe {
-        LOGGER.replace(Logger { flash: Mutex::new(RefCell::new(Some(flash))) });
+        LOGGER.replace(Logger {
+            flash: Mutex::new(RefCell::new(Some(flash))),
+        });
     }
     Ok(())
 }
@@ -69,51 +74,103 @@ impl Logger {
         {
             let mask = primask::read();
             cortex_m::interrupt::disable();
-            if let Some(flash) = self.flash.borrow(unsafe { &CriticalSection::new() }).borrow_mut().as_mut() {
-                let _ =
-                    queue::push(flash, LOGS_FLASH_RANGE, &mut NoCache::new(), &buffer, false).await;
+            if let Some(mut flash) = self
+                .flash
+                .borrow(unsafe { &CriticalSection::new() })
+                .try_borrow_mut()
+                .ok()
+            {
+                if let Some(flash) = flash.as_mut() {
+                    let _ =
+                        queue::push(flash, LOGS_FLASH_RANGE, &mut NoCache::new(), &buffer, false)
+                            .await;
+                }
                 // Ignore the result, we can't do anything about it.
+                // If we can't borrow the flash, just discard the log.
             }
-            if mask.is_active() { unsafe { cortex_m::interrupt::enable(); }}
+            if mask.is_active() {
+                unsafe {
+                    cortex_m::interrupt::enable();
+                }
+            }
         };
     }
 
-
     pub async fn get_logs<'a>(&'a mut self, f: impl Fn(&'_ str)) {
-        {
-            let mask = primask::read();
-            cortex_m::interrupt::disable();
-            if let Some(flash) = self.flash.borrow(unsafe { &CriticalSection::new() }).borrow_mut().as_mut() {
-                let mut no_cache = NoCache::new();
-                let mut iterator = queue::iter(flash, LOGS_FLASH_RANGE, &mut no_cache).await.unwrap();
-                
-                let mut buffer = [0u8; 2048];
-                let next = iterator.next(&mut buffer).await;
-                while let Ok(Some(ref buf)) = next {
-                    let s = unsafe { core::str::from_utf8_unchecked(buf) };
-                    f(s);
+        let mask = primask::read();
+        cortex_m::interrupt::disable();
+        loop {
+            let cs = unsafe { CriticalSection::new() };
+            let rc = self.flash.borrow(&cs);
+            let Ok(mut flash) = rc.try_borrow_mut() else {
+                // Another task is using the flash, we can yield and try again.
+                if mask.is_active() {
+                    unsafe {
+                        cortex_m::interrupt::enable();
+                    }
                 }
+                YieldFuture::new().await;
+                cortex_m::interrupt::disable();
+                continue;
+            };
+            let Some(flash) = flash.as_mut() else {
+                break;
+            };
+            let mut no_cache = NoCache::new();
+            let mut iterator = queue::iter(flash, LOGS_FLASH_RANGE, &mut no_cache)
+                .await
+                .unwrap();
+
+            let mut buffer = [0u8; 2048];
+            let next = iterator.next(&mut buffer).await;
+            while let Ok(Some(ref buf)) = next {
+                let s = unsafe { core::str::from_utf8_unchecked(buf) };
+                f(s);
             }
-            if mask.is_active() { unsafe { cortex_m::interrupt::enable(); }}
-        };
+        }
+        if mask.is_active() {
+            unsafe {
+                cortex_m::interrupt::enable();
+            }
+        }
     }
 
     pub async fn edit_config(&mut self, key: &ConfigKey, value: u64) {
-        {
-            let mask = primask::read();
-            cortex_m::interrupt::disable();
-            if let Some(flash) = self.flash.borrow(unsafe { &CriticalSection::new() }).borrow_mut().as_mut() {
-                let _ = map::store_item(
-                    flash,
-                    CONFIG_FLASH_RANGE,
-                    &mut NoCache::new(),
-                    &mut [0u8; 2048],
-                    key,
-                    &value,
-                )
-                .await;
+        let mask = primask::read();
+        cortex_m::interrupt::disable();
+        loop {
+            let cs = unsafe { CriticalSection::new() };
+            let rc = self.flash.borrow(&cs);
+            let Ok(mut flash) = rc.try_borrow_mut() else {
+                // Another task is using the flash, we can yield and try again.
+                if mask.is_active() {
+                    unsafe {
+                        cortex_m::interrupt::enable();
+                    }
+                }
+                YieldFuture::new().await;
+                cortex_m::interrupt::disable();
+                continue;
+            };
+            let Some(flash) = flash.as_mut() else {
+                break;
+            };
+            let _ = map::store_item(
+                flash,
+                CONFIG_FLASH_RANGE,
+                &mut NoCache::new(),
+                &mut [0u8; 2048],
+                key,
+                &value,
+            )
+            .await;
+            break;
+        }
+
+        if mask.is_active() {
+            unsafe {
+                cortex_m::interrupt::enable();
             }
-            if mask.is_active() { unsafe { cortex_m::interrupt::enable(); }}
-        };
+        }
     }
 }
